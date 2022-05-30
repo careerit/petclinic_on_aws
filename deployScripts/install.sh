be12147f53dcf067d4c8045d4fd4017c15474d3b
Kiran <kiran@yahoo.com>
2022-05-30 22:49:47 +0530

    debugging

--- deployScripts/install.sh
+++ deployScripts/install.sh
@@ -1,3 +1,6 @@
-sudo cp petclinic.war /opt/tomcat/webapps/
+sudo ls -l
+sudo cp ./petclinic.war /opt/tomcat/webapps/
+sudo ls -l /opt/tomcat/webapps/
+sudo chown tomcat.tomcat /opt/tomcat/webapps/petclinic* -R
 sudo systemctl restart tomcat.service
 
